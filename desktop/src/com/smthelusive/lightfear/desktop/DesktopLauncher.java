165ed3e9734330c87545453c1faf3f10fc4461f2
Nataliia Dziubenko <nataliia.dziubenko@lunatech.com>
2018-06-20 16:49:54 +0200

    initial commit

---
+++ desktop/src/com/smthelusive/lightfear/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.smthelusive.lightfear.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.smthelusive.lightfear.LightFearGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LightFearGame(), config);
+	}
+}
---
+++ ios/src/com/smthelusive/lightfear/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.smthelusive.lightfear;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.smthelusive.lightfear.LightFearGame;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new LightFearGame(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
